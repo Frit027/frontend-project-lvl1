59d75ed507bc0240ba79a4fe9c01300d39287cc1
Frit027 <Frit_027@mail.ru>
2022-03-02 14:45:27 +0300

    Revert "file added"
    This reverts commit 19120908e4c18cc4ad1fd5af75d057b05a775b83.

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
-  // eslint-disable-next-line no-console
+export const greeting = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  // eslint-disable-next-line no-console
   console.log(`Hello, ${userName}!`);
 };
