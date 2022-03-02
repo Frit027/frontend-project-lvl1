79d26a594d6f98af4ea3bf1d6ecffd9012af53db
Frit027 <Frit_027@mail.ru>
2022-03-02 14:49:53 +0300

    disabled two eslint-rules

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export const greeting = () => {
+export default () => {
+  // eslint-disable-next-line no-console
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
+  // eslint-disable-next-line no-console
   console.log(`Hello, ${userName}!`);
 };
