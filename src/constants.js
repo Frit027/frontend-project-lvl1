32845f38c43487dbad3359c7105e757877cf97c2
Frit027 <Frit_027@mail.ru>
2022-04-07 00:15:16 +0300

    created constants

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/prefer-default-export
+export const typeGames = {
+  EVEN: 'EVEN',
+  CALC: 'CALC',
+  GCD: 'GCD',
+  PROGRESSION: 'PROGRESSION',
+};
+
+export const countRounds = 3;
