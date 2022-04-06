8f2b625c829a88bc0408ac7830dbede9567105f8
Frit027 <Frit_027@mail.ru>
2022-04-06 20:07:32 +0300

    games moved to 'games' directory

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,22 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import greeting from '../src/cli.js';
-import { isEqual, showMessage, getRandomInteger } from '../src/index.js';
+import startGame from '../src/index.js';
 
-(function () {
-  const userName = greeting();
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-  for (let i = 0; i < 3; i += 1) {
-    const randomInt = getRandomInteger(1, 100);
-    console.log(`Question: ${randomInt}`);
-
-    const answer = readlineSync.question('Your answer: ');
-    const correctAnswer = randomInt % 2 ? 'no' : 'yes';
-    const result = isEqual(answer, correctAnswer);
-
-    showMessage(result, userName, answer, correctAnswer);
-    if (!result) return;
-  }
-  console.log(`Congratulations, ${userName}!`);
-}());
+startGame('EVEN');
