ebafea9f780142ade681f9bd0d54d66d289f7f23
Frit027 <Frit_027@mail.ru>
2022-04-07 11:01:02 +0300

    added constant PRIME

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,9 @@
-// eslint-disable-next-line import/prefer-default-export
 export const typeGames = {
   EVEN: 'EVEN',
   CALC: 'CALC',
   GCD: 'GCD',
   PROGRESSION: 'PROGRESSION',
+  PRIME: 'PRIME',
 };
 
 export const countRounds = 3;
