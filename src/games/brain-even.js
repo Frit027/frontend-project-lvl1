41389987f5acddb09a93131acff371f369de9032
Frit027 <Frit_027@mail.ru>
2022-04-11 22:16:36 +0300

    game-files renamed

--- src/games/brain-even.js
+++
@@ -1,7 +0,0 @@
-import getRandomNumber from '../random-number.js';
-import { numbers } from '../constants.js';
-
-export default () => {
-  const randomNum = getRandomNumber(numbers.rangeStart, numbers.rangeEnd);
-  return [randomNum, (randomNum % 2 ? 'no' : 'yes')];
-};
